fefc576e06b358bc7b39fd3068376f598f81bac2
Aldwin Tablante <aldwinotablante@gmail.com>
2017-10-12 00:01:25 +0800

    Initial

--- app/src/main/java/aldwin/tablante/com/mobilefinals/TempData.java
+++ app/src/main/java/aldwin/tablante/com/mobilefinals/TempData.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
@@ -16,16 +17,25 @@ import android.provider.MediaStore;
 public class TempData extends AppCompatActivity{
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private  int RESULT_LOAD_IMAGE = 1;
-
+    private int  idpic = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_wish);
     }
+public void SubmitWish(View view){
+    EditText edt1 = (EditText) findViewById(R.id.Edt1);
+    EditText edt2 = (EditText) findViewById(R.id.Edt2);
+    EditText edt3 = (EditText) findViewById(R.id.Edt3);
+    ImageView imageView = (ImageView) findViewById(R.id.imagepic);
+    DataBaseHandler db = new DataBaseHandler(this);
 
+    db.addContact(new Wishbase(edt1.getText().toString(),edt2.getText().toString(),
+            Double.parseDouble(edt3.getText().toString()),idpic));
+idpic++;
+}
      public void ViewCamera(View view){
-         DataBaseHandler db = new DataBaseHandler(this);
-       db.addContact(new Wishbase());
+
        // Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
        //  startActivityForResult(i, RESULT_LOAD_IMAGE);
          /*Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
